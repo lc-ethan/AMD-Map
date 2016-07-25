0f5bb3929f3a22f6aac8aed5927129c2de4e8735
Ethan Li <lc.ethan21@gmail.com>
2016-07-25 22:44:54 +1200

    update dependency between region selection and time series chart

--- tools/server - time series.R
+++ tools/server - time series.R
@@ -1,25 +1,65 @@
 ### time series ----
+## data creation for time series chart
+df.md_meta_ts <- df.db_meta %>% 
+  filter(proc %in% "data_ts")
+
+for (i in df.md_meta_ts$name) {
+  local({
+    vt.name_cur <- i
+    vt.group_cur <- df.md_meta_ts %>% 
+      filter(name %in% vt.name_cur) %>% 
+      .[["group"]]
+    
+    vt.trigger <- paste(vt.group_cur, vt.name_cur, sep = "$")
+    
+    observeEvent(eval(parse(text = vt.trigger)), {
+      vt.param_region <- md_base$param_region_select
+      vt.param_project <- input$param_project_select
+      
+      if (vt.param_region %in% c("region_selected", "")) {
+        md_base$data_ts <- df.db_input %>%
+          filter(PROJECTION %in% vt.param_project) %>% 
+          group_by(YEAR) %>%
+          summarise(INDEX = sum(MD, na.rm = TRUE))
+      }
+      else {
+        md_base$data_ts <- df.db_input %>%
+          filter(TA %in% vt.param_region) %>% 
+          filter(PROJECTION %in% vt.param_project) %>% 
+          group_by(YEAR) %>%
+          summarise(INDEX = sum(MD, na.rm = TRUE))
+      }
+    })
+  })
+}
 
 output$md_ts <- renderHighchart({
   df.input <- md_base$data_ts
   vt.param_time <- input$param_time_select
+  vt.param_region <- md_base$param_region_select
+  
+  vt.title_region <- ifelse(vt.param_region %in% c("", "region_selected"), "NZ", vt.param_region)
+  vt.title_region <- gsub("district", "", vt.title_region)
+  vt.title_region <- gsub("city", "", vt.title_region)
+  vt.title_region <- convert_cap(vt.title_region)
+  vt.title <- paste0("<b>Prevalence Prediction of AMD with European and Asian Combined (45-85 Years) in ", 
+                     vt.title_region, "</b>")
   
   df.plot <- df.input %>%
     mutate(color = ifelse(YEAR %in% vt.param_time, "red", "orange")) %>% 
     change_names("INDEX", "y", reminder = FALSE) %>% 
-    highcharter::list.parse3()
+    highcharter::list_parse()
   
   hc.hist <- highchart(height = 250, width = 100) %>% 
     hc_chart(type = "column") %>% 
-    #hc_title(text = paste("Prevanlence Estimate", paste(range(vt.param_time_select), collapse = " - "))) %>%
-    hc_subtitle(text = "<b>Prevanlence Estimate with European and Asian Combined (45-85 Years)</b>") %>% 
+    hc_subtitle(text = vt.title) %>% 
     hc_xAxis(title = list(text = "Time"), 
              categories = df.input$YEAR) %>% 
-    hc_yAxis(title = list(text = "Prevanlence Estimate (#)")) %>% 
+    hc_yAxis(title = list(text = "Prevalence Estimate (#)")) %>% 
     hc_add_series(data = df.plot) %>% 
     hc_tooltip(
       headerFormat = "<b>{point.x}</b> <br>",
-      pointFormat = paste0("Prevanlence Estimate (#)", ": <b>{point.y:,.0f}</b>")
+      pointFormat = paste0("Prevalence Estimate (#)", ": <b>{point.y:,.0f}</b>")
     ) %>% 
     hc_plotOptions(
       series = list(
