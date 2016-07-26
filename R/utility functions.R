a43bf09723de5dd8696b8fe1bbdb6560879e5376
Ethan Li <lc.ethan21@gmail.com>
2016-07-27 11:09:14 +1200

    include change_names() for deployment

--- R/utility functions.R
+++ R/utility functions.R
@@ -11,3 +11,44 @@ gClip <- function(shp, bb){
   
   gIntersection(shp, b_poly, byid = T)
 }
+
+change_names <- function(df, from, to, reminder = TRUE) {
+  
+  ## error checking
+  # check if from exist in df
+  
+  if (!all(from %in% colnames(df))) {
+    stop ("undefined column names selected")
+  }
+  
+  # check if the length of the mapping match
+  len.from <- length(from)
+  len.to <- length(to)
+  
+  if (len.from != len.to) {
+    stop("argument imply differing length of vectors:", len.from, ",", len.to)
+  }
+  
+  org.from <- from
+  org.to <- to
+  
+  ## process to modify the specified names
+  field.names <- names(df)
+  field.names <- field.names[!(field.names %in% from)]
+  
+  to <- c(to, field.names)
+  from <- c(from, field.names)
+  
+  names(to) <- from
+  names(df) <- to[names(df)]
+  
+  if (reminder) {
+    cat("the column name(s) have been modified:\n")
+    indicator <- paste(org.from, " to ", org.to, "\n", sep = "")
+    indicator[1] <- paste(" ", indicator[1], sep = "")
+    cat(indicator)
+  }  
+  
+  return(df)
+  
+}
